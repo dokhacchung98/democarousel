75accecc40bd796abacbe3f55ed4f10e8cd80631
khacchungory <ochung3@gmail.com>
2020-06-05 14:59:28 +0700

    demo carousel

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import DemoCarousel from './page/DemoCarousel'
+
+export default class App extends Component {
+  render() {
+    return (
+      <DemoCarousel />
+    )
+  }
+}
